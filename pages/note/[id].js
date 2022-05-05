02845aa36e036824b5cc4aa2034b7d362f92ac61
Alex <ssaasshha11@yandex.ru>
2022-05-05 14:18:50 +0400

    change

--- pages/note/[id].js
+++ pages/note/[id].js
@@ -6,25 +6,6 @@ import { useState, useContext } from "react";
 import noteContext from "../../components/context/noteContext";
 import { useRouter } from "next/router";
 
-export async function getStaticPaths() {
-	const uri =
-		"https://severstal-7dd9c-default-rtdb.firebaseio.com/Notes.json";
-	const response = await axios.get(uri);
-	if (response.status !== 200) return;
-	const transformedData = Object.keys(response.data).map((key) =>
-		!response.data[key]
-			? null
-			: {
-					params: { id: key },
-			  }
-	);
-	const paths = transformedData.filter((entity) => (entity ? true : false));
-	return {
-		paths,
-		fallback: false,
-	};
-}
-
 export async function getServerSideProps({ params }) {
 	const { id } = params;
 	const uri = `https://severstal-7dd9c-default-rtdb.firebaseio.com/Notes/${id}.json`;
